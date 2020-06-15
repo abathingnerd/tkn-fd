479549fd0ee897b869ade62e89ec6e1fe2bc9d2a
abathingnerd <59466021+abathingnerd@users.noreply.github.com>
2020-06-14 22:44:40 -0700

    Revert "Revert "big commit""
    This reverts commit 4e8030cf357aad83e32d24ca929cd74cb2cf036f.

---
+++ script.js
@@ -0,0 +1,65 @@
+// Event listener when clicking on character card.
+// document.querySelector('.card').addEventListener("click", function(){
+//     window.alert('Hit');
+// });
+
+//Event listener when closing the character modal.
+// document.querySelector('.close').addEventListener('click', function(){
+//     document.querySelector('.character-info-window-overlay').style.display='none';
+// });
+
+const app = document.getElementById('character-select');
+// console.log(app);
+
+// const container = document.createElement('div');
+// container.setAttribute('class', 'container');
+
+const sidebar = document.getElementById('sidebar');
+
+// app.appendChild(container);
+
+/*THIS CODE MAKES THE REQUEST*/
+var request = new XMLHttpRequest()
+var proxy = 'https://cors-anywhere.herokuapp.com/'
+
+// request.open('GET', proxy+'http://t7api.herokuapp.com/character/list', true)
+
+request.open('GET', 'http://localhost:3000/character', true)
+
+request.onload = function() {
+    var data = JSON.parse(this.response)
+    let characterLabels = data.map ( character => character.label)
+
+    labels = characterLabels.sort();
+
+    characterLabels.forEach( label => {
+
+        //Create div element for card
+        const card = document.createElement('div');
+        card.setAttribute('class', 'card');
+        card.setAttribute('id', label);
+
+        //Create image element for card
+        const thumbnail = document.createElement('img');
+        thumbnail.setAttribute('src', 'images/thumbnails/'+label+'.jpg');
+
+        //create div element for name
+        const characterName = document.createElement('div');
+        characterName.setAttribute('class', 'card-name');
+        // characterName.setAttribute('id', 'card-'+name);
+        characterName.textContent = (label);
+
+        app.appendChild(card);
+        card.appendChild(thumbnail);
+        card.appendChild(characterName);
+        // console.log(name);
+
+        //create div to show text for testing
+        const someText = document.createElement('div');
+        someText.textContent = ('text');
+        sidebar.appendChild(someText);
+    });
+}
+
+//Send Request
+request.send()
