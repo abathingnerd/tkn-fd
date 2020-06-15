e2a51f40bc13493021812a8cb9bdeabb174c67c5
abathingnerd <59466021+abathingnerd@users.noreply.github.com>
2020-06-14 23:05:26 -0700

    clean up
    - added createCard script
    - cleaned up index.html and script.js files
    - added character names to display with an ellipsis if text-overflow

--- script.js
+++ script.js
@@ -18,48 +18,48 @@ const sidebar = document.getElementById('sidebar');
 
 // app.appendChild(container);
 
-/*THIS CODE MAKES THE REQUEST*/
-var request = new XMLHttpRequest()
-var proxy = 'https://cors-anywhere.herokuapp.com/'
+// /*THIS CODE MAKES THE REQUEST*/
+// var request = new XMLHttpRequest()
+// var proxy = 'https://cors-anywhere.herokuapp.com/'
 
-// request.open('GET', proxy+'http://t7api.herokuapp.com/character/list', true)
+// // request.open('GET', proxy+'http://t7api.herokuapp.com/character/list', true)
 
-request.open('GET', 'http://localhost:3000/character', true)
+// request.open('GET', 'http://localhost:3000/character', true)
 
-request.onload = function() {
-    var data = JSON.parse(this.response)
-    let characterLabels = data.map ( character => character.label)
+// request.onload = function() {
+//     var data = JSON.parse(this.response)
+//     let characterLabels = data.map ( character => character.label)
 
-    labels = characterLabels.sort();
+//     labels = characterLabels.sort();
 
-    characterLabels.forEach( label => {
+//     characterLabels.forEach( label => {
 
-        //Create div element for card
-        const card = document.createElement('div');
-        card.setAttribute('class', 'card');
-        card.setAttribute('id', label);
+//         //Create div element for card
+//         const card = document.createElement('div');
+//         card.setAttribute('class', 'card');
+//         card.setAttribute('id', label);
 
-        //Create image element for card
-        const thumbnail = document.createElement('img');
-        thumbnail.setAttribute('src', 'images/thumbnails/'+label+'.jpg');
+//         //Create image element for card
+//         const thumbnail = document.createElement('img');
+//         thumbnail.setAttribute('src', 'images/thumbnails/'+label+'.jpg');
 
-        //create div element for name
-        const characterName = document.createElement('div');
-        characterName.setAttribute('class', 'card-name');
-        // characterName.setAttribute('id', 'card-'+name);
-        characterName.textContent = (label);
+//         //create div element for name
+//         const characterName = document.createElement('div');
+//         characterName.setAttribute('class', 'card-name');
+//         // characterName.setAttribute('id', 'card-'+name);
+//         characterName.textContent = (label);
 
-        app.appendChild(card);
-        card.appendChild(thumbnail);
-        card.appendChild(characterName);
-        // console.log(name);
+//         app.appendChild(card);
+//         card.appendChild(thumbnail);
+//         card.appendChild(characterName);
+//         // console.log(name);
 
-        //create div to show text for testing
-        const someText = document.createElement('div');
-        someText.textContent = ('text');
-        sidebar.appendChild(someText);
-    });
-}
+//         //create div to show text for testing
+//         const someText = document.createElement('div');
+//         someText.textContent = ('text');
+//         sidebar.appendChild(someText);
+//     });
+// }
 
-//Send Request
-request.send()
+// //Send Request
+// request.send()
