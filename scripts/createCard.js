07de5d5736ebb1c9a440d702ee6ab53d6bdfcead
abathingnerd <59466021+abathingnerd@users.noreply.github.com>
2020-06-18 03:18:20 -0700

    minor-update

--- scripts/createCard.js
+++ scripts/createCard.js
@@ -6,27 +6,30 @@ request.open("GET", "http://192.168.1.25:3000/character", true);
 request.onload = function () {
   let data = JSON.parse(this.response);
 
-  let characterLabels = data.map((character) => character.label);
-  labels = characterLabels.sort();
+  let names = data.map((character) => character.name);
+
+  characterNames = names.sort();
+
+  // names = characterNames.sort();
 
   //GRAB PARENT ELEMENT
   const characterSelect = document.getElementById("character-select");
 
   //FOREACH LOOP TO CREATE A CARD FOR EACH CHARACTER
-  characterLabels.forEach((label) => {
+  characterNames.forEach((name) => {
     //CREATE DIV FOR CARD
     const card = document.createElement("div");
     card.setAttribute("class", "card");
-    card.setAttribute("id", label);
+    card.setAttribute("id", name);
 
     //CREATE IMAGE FOR CARD
     const thumbnail = document.createElement("img");
-    thumbnail.setAttribute("src", "images/card/" + label + ".jpg");
+    thumbnail.setAttribute("src", "images/card/" + name + ".jpg");
 
     //CREATE DIV FOR NAME
     const characterName = document.createElement("div");
     characterName.setAttribute("class", "card-name");
-    characterName.textContent = label;
+    characterName.textContent = name.split("-").join(" ");
 
     //STRUCTURE ELEMENTS FOR CARD
     characterSelect.appendChild(card);
@@ -34,8 +37,8 @@ request.onload = function () {
     card.appendChild(characterName);
 
     //CLICKING CHARACTER GOES TO THE RIGHT PAGE
-    document.getElementById(label).onclick = function () {
-      location.href = `${label}.html`.toLowerCase();
+    document.getElementById(name).onclick = function () {
+      location.href = `${name}.html`.toLowerCase();
     };
   });
 };
